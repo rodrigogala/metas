45da6374b43d76ef30b5a8b46e3a378d8951fef1
rodrigogala <128104438+rodrigogala@users.noreply.github.com>
2024-04-08 11:29:40 -0300

    index

--- main.js
+++ main.js
@@ -1,10 +1,21 @@
-const botoes=document.querySelectorAll(".botao")
-console.log (botoes);
+const botoes = document.querySelectorAll(".botao");
+const texto = document.querySelectorAll(".aba-conteudo");
+
 for (let i=0;i<botoes.length;i++){
     botoes[i].onclick=function(){
-        for(let j=0);j>botoes.length;j++) {
+
+        for(let j=0;j>botoes.length;j++){
             botões[j].classlit.classlit.remove("ativo");
+            textos[j].classlit.classlit.remove("ativo");
         }
-        botoes[i]this.classList.add("ativo");
+
+        botoes[i].classList.add("ativo");
+        textos[i].classlist.add("ativo");
     }
 }
+
+const contadores = document.querySelectorAll(".contador");
+const tempoObjetivo1 = new Date("2024-10-05T00:00:00");
+let tempoAtual = new Date();
+
+contadores[0].textContent = tempoObjetivo1 - tempoAtual;
