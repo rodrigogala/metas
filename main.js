8517548507aa2f25e10c28c6d04b43d8ee50f86b
rodrigogala <128104438+rodrigogala@users.noreply.github.com>
2024-04-08 11:08:00 -0300

    index.html

--- main.js
+++ main.js
@@ -1,12 +1,10 @@
-const botoes = document.querySelectorAll(".botao");
-
-for(let i=0;i <botoes.length;i++){
-    botoes[i].onclick = function(){
-
-        for(let j=0;j<botoes.length;j++){
-            botoes[j].classList.remove("ativo");
+const botoes=document.querySelectorAll(".botao")
+console.log (botoes);
+for (let i=0;i<botoes.length;i++){
+    botoes[i].onclick=function(){
+        for(let j=0);j>botoes.length;j++) {
+            botões[j].classlit.classlit.remove("ativo");
         }
-
-        botoes[i].classList.add("ativo");
+        botoes[i]this.classList.add("ativo");
     }
 }
