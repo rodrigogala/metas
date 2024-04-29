89bd57db00a905302e30200777f0b90872f37925
rodrigogala <128104438+rodrigogala@users.noreply.github.com>
2024-04-29 10:55:07 -0300

    style.css

--- main.js
+++ main.js
@@ -1,24 +1,24 @@
 const botoes = document.querySelectorAll(".botao");
-const texto = document.querySelectorAll(".aba-conteudo");
+const textos = document.querySelectorAll(".aba-conteudo");
 
-for (let i=0;i<botoes.length;i++){
-    botoes[i].onclick=function(){
+for (let i = 0; i < botoes.length; i++) {
+    botoes[i].onclick = function () {
 
-        for(let j=0;j>botoes.length;j++){
-            botões[j].classlit.classlit.remove("ativo");
-            textos[j].classlit.classlit.remove("ativo");
+        for (let j = 0; j < botoes.length; j++) {
+            botoes[j].classList.remove("ativo");
+            textos[j].classList.remove("ativo");
         }
 
         botoes[i].classList.add("ativo");
-        textos[i].classlist.add("ativo");
+        textos[i].classList.add("ativo");
     }
 }
 
 const contadores = document.querySelectorAll(".contador");
-const tempoObjetivo1 = new Date("2024-10-05T00:00:00");
-let tempoAtual = new Date();
+const tempoObjetivo1 = new Date("2023-10-05T00:00:00");
 
-contadores[0].textContent = tempoObjetivo1 - tempoAtual;
+
+contadores[0].textContent = calculaTempo(tempoObjetivo1);
 
 function calculaTempo(tempoObjetivo) {
     let tempoAtual = new Date();
